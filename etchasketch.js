85b3e60dd069b9cdbf02fff93ca357900260934c
Capz-DH <108783601+Capz-DH@users.noreply.github.com>
2022-07-15 13:09:53 +0100

    Add files via upload
    Elements now fill their background to black with mousedown event listener

--- etchasketch.js
+++ etchasketch.js
@@ -1,12 +1,15 @@
 const container = document.getElementById("container");
 
+
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+
   };
+  
 };
 makeRows(16, 16);
 
@@ -19,3 +22,13 @@ function refreshRows(){
     clearRows();
     makeRows(16, 16);
 };
+
+document.querySelectorAll('.grid-item').forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.add('filled');
+  })
+})
+
+
+
+
